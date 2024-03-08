86b15d996044965d121193377fe0e6ac25705276
anu-kriti-Shri-vastava <anukriti13901@gmail.com>
2024-03-09 00:05:01 +0530

    error Solve

--- client/src/pages/PropertyOne/PropertyOne.jsx
+++ client/src/pages/PropertyOne/PropertyOne.jsx
@@ -14,7 +14,7 @@ import { Button } from "@mantine/core";
 import { toast } from "react-toastify";
 import Heart from "../../components/Heart/Heart";
 import "./PropertyOne.css";
-import Map from "../../components/map/Map.jsx";
+import Map from "../../components/map/Map";
 
 const PropertyOne = () => {
     const { pathname } = useLocation();
